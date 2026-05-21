51b8f076ac138c3abba8d05918a4c0a7d43f53d3
Soydaantes311 <carlosdante311@gmail.com>
2026-05-20 20:09:54 -0600

    Ejercisio 2

--- Ejercisio2.py
+++ Ejercisio2.py
@@ -2,11 +2,10 @@ import pulp
 
 model = pulp.LpProblem("Cloud_Optimization", pulp.LpMinimize)
 
-x1 = pulp.LpVariable("Instancia_A", lowBound=0, upBound=40, cat='Integer')
-x2 = pulp.LpVariable("Instancia_B", lowBound=0, upBound=30, cat='Integer')
+x1 = pulp.LpVariable("Instancia_C", lowBound=0, upBound=80, cat='Integer')
+x2 = pulp.LpVariable("Instancia_D", lowBound=0, upBound=60, cat='Integer')
 
 model += 10 * x1 + 30 * x2, "Costo_Total"
-
 model += 2 * x1 + 4 * x2 >= 100, "CPU_Demand"
 model += 1 * x1 + 5 * x2 >= 80, "RAM_Demand"
 
@@ -16,5 +15,3 @@ print(f"Estado: {pulp.LpStatus[model.status]}")
 print(f"Contratar Tipo A: {x1.varValue}")
 print(f"Contratar Tipo B: {x2.varValue}")
 print(f"Costo Mínimo Diario: ${pulp.value(model.objective)}")
-
-#source .venv/bin/activate
